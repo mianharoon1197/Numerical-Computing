e80654d5c01b6540a52ed5c2ec6836af6fee7ca4
mianharoon1197 <harooniftikhar699@gmail.com>
2025-10-27 11:58:29 +0500

    added examples

---
+++ Lab1/secentExample1.m
@@ -0,0 +1,15 @@
+f = @(x) x^3 - x - 2; % Function
+a = 1; b = 2; % Interval
+tol = 1e-5; maxIter = 100;
+for i = 1:maxIter
+c = (a+b)/2;
+if f(a)*f(c) < 0
+b = c;
+else
+a = c;
+end
+if abs(f(c)) < tol
+break
+end
+end
+disp(['Root approx = ', num2str(c)])
