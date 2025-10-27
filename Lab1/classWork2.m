7aa01666ebb314459ac6b00c41bd66bc4a99d7fa
mianharoon1197 <harooniftikhar699@gmail.com>
2025-10-27 11:57:47 +0500

    added classwork2

---
+++ Lab1/classWork2.m
@@ -0,0 +1,18 @@
+f = @(x) sin(x) - 0.5;
+a=0; b=2;
+
+tol = 1e-5; maxIter = 100;
+for i = 1:maxIter
+c = (a+b)/2;
+if f(a) * f(c) < 0
+b=c;
+else
+a=c;
+end
+if abs(f(c)) < tol
+break
+end
+end
+disp(['Root approx = ', num2str(c)])
+
+
