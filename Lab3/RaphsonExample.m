0b881bcc1631ae1291ad56803e5e8a69437bf55c
mianharoon1197 <harooniftikhar699@gmail.com>
2025-11-24 11:12:03 +0500

    Newton Raphson Method

---
+++ Lab3/RaphsonExample.m
@@ -0,0 +1,22 @@
+f = @(x) x^3- 2*x- 5;
+ df = @(x) 3*x^2- 2;
+ x0 = 2;
+ tol = 1e-6;
+ maxIter = 50;
+ % Display table header
+ fprintf('Iter\t x_n\t\t f(x_n)\t\t x_{n+1}\n');
+ fprintf('------------------------------------------------\n');
+ % Iterative process
+ for i = 1:maxIter
+ x1 = x0- f(x0)/df(x0);
+ % Newton-Raphson formula
+ % Display current iteration
+ fprintf('%d\t %.6f\t %.6f\t %.6f\n', i, x0, f(x0), x1);
+ if abs(x1- x0) < tol
+ fprintf('Converged after %d iterations.\n', i);
+ break;
+ end
+ x0 = x1; % Update for next iteration
+ end
+ % Display final result
+ fprintf('Approximate root: %.6f\n', x1);
